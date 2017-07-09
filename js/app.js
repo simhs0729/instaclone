df0c3e1ff4e814ff2c549d9daaf03394ad031c7c
simhs0729 <simhs0729@gmail.com>
2017-07-09 18:11:40 +0900

    save

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
     
     var $heart = $('.heart'),
-        $addComment = $('.photo__add-comment');
+        $addComment = $('.photo__add-comment'),
+        $ellipsis = $('.fa-ellipsis-h'),
+        $popUp = $('.popUp'),
+        $closePopup = $('.fa-times'),
+        $closePopUpBtn = $('.closePopUpBtn');
 
     $heart.click(function(){
         var likes = $(this).parent().parent()
@@ -17,17 +21,35 @@ $(document).ready(function(){
         likes.html(newValue)
         $(this).toggleClass('fa-heart-o fa-heart');
     });
-	$addComment.keydown(function(event){
-		if(event.keyCode ==13) {
-			var newComment = event.target.value;
-			var commentList = $(this).parent().parent().children('.photo__comments');
-			$(this).val('').blur();
-			commentList.append("<li class='photo__comment'><span class='photo__comment__author'>serranoarevalo</span> " +  newComment +"</li>")
-		}
-	})
+
+    $addComment.keydown(function(event){
+        // Detect if the key is enter
+        if(event.keyCode == 13){
+            // Save the textarea's value
+            var newComment = event.target.value;
+            // Select the comment list above textarea
+            var commentList = $(this).parent().parent().children('.photo__comments');
+            // Empty the value of textarea and show placehodler again
+            $(this).val('').blur();
+            // Append a <li> to photo__comments.
+            commentList.append("<li class='photo__comment'>\
+                <span class='photo__comment-author'>serranoarevalo</span> "
+            + newComment +  "</li>");
+        }
+    })
+
+    $ellipsis.click(function(){
+        //show popup
+        $popUp.fadeIn();
+    })
+
+    $closePopup.click(closePopup);
+    $closePopUpBtn.click(closePopup);
+
+    function closePopup(event){
+        event.preventDefault()
+        $popUp.fadeOut();
+    }
+
+
 })
-
-
-
-
-
