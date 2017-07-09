3bdecd8560fc3d1f3613408491ff08d5a3e0ecea
simhs0729 <simhs0729@gmail.com>
2017-07-09 16:26:08 +0900

    done?

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 
-	var $heart = $('.heart');
+	var $heart = $('.heart'),
+		$addComment = $('.photo__add-comment');
 
 	$heart.click(function(){
 		$(this).toggleClass('fa-heart-o fa-heart')
+	});
+
+	$addComment.keydown(function(event){
+		if(event.keyCode ==13) {
+			var newComment = event.target.value;
+			var commentList = $(this).parent().parent().children('.photo__comments');
+			$(this).val('').blur();
+			commentList.append("<li class='photo__comment'><span class='photo__comment__author'>serranoarevalo</span> " + newComment +"</li>")
+		}
 	})
 })
+
+
 
 
 
